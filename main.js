99665a2320a111befe5653b1fe013fd119fab59c
apenaranda <alejandropenaranda3001@gmail.com>
2023-07-04 15:17:02 -0500

    lightning

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 let scene, camera, renderer, clock, controls;
 
 const objects = [];
+const sol = [];
 
-const speed = 1.0;
+const speed = 1;
 const height = 3;
 const offset = 0.5;
 
@@ -15,7 +16,7 @@ function init() {
 
   // Creación de camara.
   camera = new THREE.PerspectiveCamera(
-    45,
+    60,
     window.innerWidth / window.innerHeight,
     0.1,
     100
@@ -23,7 +24,7 @@ function init() {
 
   // Añado imagen al fondo.
   const textureLoader = new THREE.TextureLoader();
-  scene.background = textureLoader.load("/img/stars.jpg");
+  scene.background = textureLoader.load("/img/f1.jpg");
 
   // Permito controlar la camara con el mouse.
   controls = new OrbitControls(camera, renderer.domElement);
@@ -34,13 +35,13 @@ function init() {
   controls.update();
 
   // Luz.
-  const ambientLight = new THREE.AmbientLight(0xcccccc);
+  const ambientLight = new THREE.AmbientLight(0x3A3938,1);
   scene.add(ambientLight);
 
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 2.5);
-  directionalLight.position.set(0, 5, 5);
+  const directionalLight = new THREE.DirectionalLight(0xA4A4A4 , 2);
+  directionalLight.position.set(-20, 20, 1);
   scene.add(directionalLight);
-
+  
   const d = 5;
   directionalLight.castShadow = true;
   directionalLight.shadow.camera.left = -d;
@@ -68,7 +69,7 @@ function init() {
   // Objetos.
   const ballTexture = textureLoader.load("/img/nebula.jpg");
 
-  const count = 5;
+  const count = 6;
   const radius = 3;
 
   const ballGeometry = new THREE.SphereGeometry(0.5, 32, 16);
@@ -88,6 +89,45 @@ function init() {
     scene.add(ball);
     objects.push(ball);
   }
+
+  // luces moviles
+  const bgeom = new THREE.SphereGeometry(0.1, 32, 16);
+
+  const pointLight1 = new THREE.PointLight(0xE46100,60);
+  pointLight1.position.set(0,1.5,0);
+  pointLight1.castShadow = true;
+  const bLightMat = new THREE.MeshLambertMaterial({
+    emissive: 0xE46100,
+    emissiveIntensity: 60,
+    color: 0x000000});
+
+  pointLight1.add(new THREE.Mesh(bgeom,bLightMat))
+  scene.add(pointLight1)
+  sol.push(pointLight1)
+
+  const pointLight2 = new THREE.PointLight(0x0CF00F,30);
+  pointLight2.position.set(-3.5,1.5,-3.5);
+  pointLight2.castShadow = true;
+  const bLightMat2 = new THREE.MeshLambertMaterial({
+    emissive: 0x0CF00F,
+    emissiveIntensity: 30,
+    color: 0x000000});
+
+  pointLight2.add(new THREE.Mesh(bgeom,bLightMat2))
+  scene.add(pointLight2)
+  sol.push(pointLight2)
+
+  const pointLight3 = new THREE.PointLight(0x0046FF,30);
+  pointLight3.position.set(3.5,1.5,3.5);
+  pointLight3.castShadow = true;
+  const bLightMat3 = new THREE.MeshLambertMaterial({
+    emissive: 0x0046FF,
+    emissiveIntensity: 30,
+    color: 0x000000});
+
+  pointLight3.add(new THREE.Mesh(bgeom,bLightMat3))
+  scene.add(pointLight3)
+  sol.push(pointLight3)
 
   animate();
 }
@@ -118,12 +158,27 @@ function animate() {
 // Animación.
 function render() {
   const time = clock.getElapsedTime();
+  
+  for (let i = 0; i < sol.length; i++) {
+    const luz = sol[i]
+    const previousHeight = luz.position.y;
+      luz.position.y = Math.abs(Math.sin(i * offset + time * speed) * height);
+
+      if (luz.position.y < previousHeight) {
+        luz.userData.down = true;
+      } else {
+        if (luz.userData.down === true) {
+          // ball changed direction from down to up
+          luz.userData.down = false;
+        }
+      }
+    }
 
   for (let i = 0; i < objects.length; i++) {
     const ball = objects[i];
-
+    ball.rotateY(0.004);
     const previousHeight = ball.position.y;
-    ball.position.y = Math.abs(Math.sin(i * offset + time * speed) * height);
+    ball.position.y = Math.abs(Math.sin(i*offset + time * speed) * height);
 
     if (ball.position.y < previousHeight) {
       ball.userData.down = true;
