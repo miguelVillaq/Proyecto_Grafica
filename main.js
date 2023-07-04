3396430af90fa3a972871ea992e240561743e22a
miguelVillaq <125335842+miguelVillaq@users.noreply.github.com>
2023-07-04 16:34:34 -0500

    Añado más comentarios guía

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let scene, camera, renderer, clock, controls;
 const objects = [];
 const sol = [];
 
-const speed = 1;
+const velocidad = 1;
 const height = 3;
 const offset = 0.5;
 
@@ -21,7 +21,7 @@ function init() {
     0.1,
     100
   );
-
+  console.log("width", window.innerWidth, "hi", window.innerHeight)
   // Añado imagen al fondo.
   const textureLoader = new THREE.TextureLoader();
   scene.background = textureLoader.load("/img/f1.jpg");
@@ -29,31 +29,31 @@ function init() {
   // Permito controlar la camara con el mouse.
   controls = new OrbitControls(camera, renderer.domElement);
   controls.minDistance = 1;
-  controls.maxDistance = 25;
+  controls.maxDistance = 15;
 
   camera.position.set(7, 3, 7);
   controls.update();
 
   // Luz.
-  const ambientLight = new THREE.AmbientLight(0x3A3938,1);
+  const ambientLight = new THREE.AmbientLight(0x3a3938, 1);
   scene.add(ambientLight);
 
-  const directionalLight = new THREE.DirectionalLight(0xA4A4A4 , 2);
+  const directionalLight = new THREE.DirectionalLight(0xa4a4a4, 2);
   directionalLight.position.set(-20, 20, 1);
   scene.add(directionalLight);
-  
-  const d = 5;
+
+  const distance = 3;
   directionalLight.castShadow = true;
-  directionalLight.shadow.camera.left = -d;
-  directionalLight.shadow.camera.right = d;
-  directionalLight.shadow.camera.top = d;
-  directionalLight.shadow.camera.bottom = -d;
+  directionalLight.shadow.camera.left = -distance;
+  directionalLight.shadow.camera.right = distance;
+  directionalLight.shadow.camera.top = distance;
+  directionalLight.shadow.camera.bottom = -distance;
 
   directionalLight.shadow.camera.near = 1;
   directionalLight.shadow.camera.far = 20;
 
-  directionalLight.shadow.mapSize.x = 1024;
-  directionalLight.shadow.mapSize.y = 1024;
+  directionalLight.shadow.mapSize.x = window.innerWidth;
+  directionalLight.shadow.mapSize.y = window.innerWidth;
 
   // Suelo.
   const floorTexture = textureLoader.load("/img/chess.jpg");
@@ -90,44 +90,47 @@ function init() {
     objects.push(ball);
   }
 
-  // luces moviles
+  // Luces moviles
   const bgeom = new THREE.SphereGeometry(0.1, 32, 16);
 
-  const pointLight1 = new THREE.PointLight(0xE46100,60);
-  pointLight1.position.set(0,1.5,0);
+  const pointLight1 = new THREE.PointLight(0xe46100, 60);
+  pointLight1.position.set(0, 1.5, 0);
   pointLight1.castShadow = true;
   const bLightMat = new THREE.MeshLambertMaterial({
-    emissive: 0xE46100,
+    emissive: 0xe46100,
     emissiveIntensity: 60,
-    color: 0x000000});
+    color: 0x000000,
+  });
 
-  pointLight1.add(new THREE.Mesh(bgeom,bLightMat))
-  scene.add(pointLight1)
-  sol.push(pointLight1)
+  pointLight1.add(new THREE.Mesh(bgeom, bLightMat));
+  scene.add(pointLight1);
+  sol.push(pointLight1);
 
-  const pointLight2 = new THREE.PointLight(0x0CF00F,30);
-  pointLight2.position.set(-3.5,1.5,-3.5);
+  const pointLight2 = new THREE.PointLight(0x0cf00f, 30);
+  pointLight2.position.set(-3.5, 1.5, -3.5);
   pointLight2.castShadow = true;
   const bLightMat2 = new THREE.MeshLambertMaterial({
-    emissive: 0x0CF00F,
+    emissive: 0x0cf00f,
     emissiveIntensity: 30,
-    color: 0x000000});
+    color: 0x000000,
+  });
 
-  pointLight2.add(new THREE.Mesh(bgeom,bLightMat2))
-  scene.add(pointLight2)
-  sol.push(pointLight2)
+  pointLight2.add(new THREE.Mesh(bgeom, bLightMat2));
+  scene.add(pointLight2);
+  sol.push(pointLight2);
 
-  const pointLight3 = new THREE.PointLight(0x0046FF,30);
-  pointLight3.position.set(3.5,1.5,3.5);
+  const pointLight3 = new THREE.PointLight(0x0046ff, 30);
+  pointLight3.position.set(3.5, 1.5, 3.5);
   pointLight3.castShadow = true;
   const bLightMat3 = new THREE.MeshLambertMaterial({
-    emissive: 0x0046FF,
+    emissive: 0x0046ff,
     emissiveIntensity: 30,
-    color: 0x000000});
+    color: 0x000000,
+  });
 
-  pointLight3.add(new THREE.Mesh(bgeom,bLightMat3))
-  scene.add(pointLight3)
-  sol.push(pointLight3)
+  pointLight3.add(new THREE.Mesh(bgeom, bLightMat3));
+  scene.add(pointLight3);
+  sol.push(pointLight3);
 
   animate();
 }
@@ -142,6 +145,7 @@ document.body.appendChild(renderer.domElement);
 
 window.addEventListener("resize", onWindowResize);
 
+// Ajustar a pestaña.
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
@@ -158,33 +162,33 @@ function animate() {
 // Animación.
 function render() {
   const time = clock.getElapsedTime();
-  
-  for (let i = 0; i < sol.length; i++) {
-    const luz = sol[i]
-    const previousHeight = luz.position.y;
-      luz.position.y = Math.abs(Math.sin(i * offset + time * speed) * height);
 
-      if (luz.position.y < previousHeight) {
-        luz.userData.down = true;
-      } else {
-        if (luz.userData.down === true) {
-          // ball changed direction from down to up
-          luz.userData.down = false;
-        }
+  for (let i = 0; i < sol.length; i++) {
+    const luz = sol[i];
+    const previousHeight = luz.position.y;
+    luz.position.y = Math.abs(Math.sin(i * offset + time * velocidad) * height);
+
+    if (luz.position.y < previousHeight) {
+      luz.userData.down = true;
+    } else {
+      if (luz.userData.down === true) {
+        // Bola de abajo hacía arriba.
+        luz.userData.down = false;
       }
     }
+  }
 
   for (let i = 0; i < objects.length; i++) {
     const ball = objects[i];
     ball.rotateY(0.004);
     const previousHeight = ball.position.y;
-    ball.position.y = Math.abs(Math.sin(i*offset + time * speed) * height);
+    ball.position.y = Math.abs(Math.sin(i * offset + time * velocidad) * height);
 
     if (ball.position.y < previousHeight) {
       ball.userData.down = true;
     } else {
       if (ball.userData.down === true) {
-        // ball changed direction from down to up
+        // Bola de arriba hacía abajo.
         ball.userData.down = false;
       }
     }
@@ -193,4 +197,5 @@ function render() {
   renderer.render(scene, camera);
 }
 
+// Realiza la ejecución.
 init();
